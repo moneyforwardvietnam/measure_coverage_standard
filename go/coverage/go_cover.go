a8d63b57c7ed4a88b55ca4371dc32f2fd4c03e43
Visu <kuppusamy.suganya@moneyforward.co.jp>
2023-08-24 13:09:32 +0530

    Revert "CR fix"
    This reverts commit 04a36c95b85acdc98d65cad5cab35fe925083ada.

--- go/coverage/go_cover.go
+++ go/coverage/go_cover.go
@@ -3,6 +3,7 @@ package coverage
 func CheckNumber(a int) string {
 	if a > 0 && a%2 == 0 {
 		return "Number is positive and even"
+	} else {
+		return "Number is Negative or odd"
 	}
-	return "Number is Negative or odd"
 }
