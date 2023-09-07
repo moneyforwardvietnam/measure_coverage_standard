b71a4ae2b11b4452f993ed47cc6fbe2931bea24d
Visu <kuppusamy.suganya@moneyforward.co.jp>
2023-09-07 16:01:05 +0530

    OpenClover report added

---
+++ java/results/site_clover_ALL_TC/clover/treemap-json.js
@@ -0,0 +1,9 @@
+var treeMapJson = {"id":"Clover database Thu Sep 7 2023 15:56:51 IST0","name":"","data":{
+    "$area":6.0,"$color":100.0,"title":" 6 Elements, 100% Coverage"},
+  "children":[{"id":"example0","name":"example","data":{"$area":6.0,"$color":
+        100.0,"title":"example 6 Elements, 100% Coverage"},"children":[{"id":
+          "JavaCoverage0","name":"JavaCoverage","data":{"$area":6.0,"$color":
+            100.0,"path":"example/JavaCoverage.html#JavaCoverage","title":
+            "JavaCoverage 6 Elements, 100% Coverage"},"children":[]}]}]}
+;
+processTreeMapJson (treeMapJson);
